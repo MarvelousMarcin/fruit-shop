e14f39922c84ed0bc433b2eef1394570c827ab1d
MarvelousMarcin <01159356@pw.edu.pl>
2023-08-29 19:18:05 +0200

    checkout

--- app/basket/components/BasketContent.tsx
+++ app/basket/components/BasketContent.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { useStore } from "@/app/store-global/store";
-import { FC } from "react";
+import { Dispatch, FC, SetStateAction } from "react";
 import BasketProduct from "./BasketProduct";
 
-interface BasketContentProps {}
+interface BasketContentProps {
+  setIsCheckout: Dispatch<SetStateAction<boolean>>;
+}
 
-const BasketContent: FC<BasketContentProps> = ({}) => {
+const BasketContent: FC<BasketContentProps> = ({ setIsCheckout }) => {
   const { basket } = useStore();
+  const price = basket.reduce(
+    (acc, curr) => acc + curr.price * curr.quantity,
+    0
+  );
   return (
     <>
-      <div className="w-screen h-fit flex flex-col justify-between px-6 gap-14">
+      <div className="w-screen  h-fit flex flex-col justify-between px-6 gap-14 mb-36">
         {basket.map((prod) => (
           <>
             <BasketProduct product={prod} />
@@ -17,6 +23,18 @@ const BasketContent: FC<BasketContentProps> = ({}) => {
           </>
         ))}
       </div>
+      <section className="h-36 w-screen bg-white  fixed bottom-0 left-0 px-20 flex justify-between items-center">
+        <section>
+          <h1 className="text-2xl text-orange-main font-bold">Total: </h1>
+          <p className="text-xl fonrtt-bold">$ {price}</p>
+        </section>
+        <button
+          onClick={() => setIsCheckout((prev) => !prev)}
+          className="bg-orange-main w-56 text-white p-4 z-20 font-bold rounded-md tracking-wide"
+        >
+          Checkout
+        </button>
+      </section>
     </>
   );
 };
--- app/basket/page.tsx
+++ app/basket/page.tsx
@@ -1,13 +1,55 @@
 "use client";
 
-import { ArrowBigLeft } from "lucide-react";
+import { ArrowBigLeft, X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import BasketContent from "./components/BasketContent";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import Input from "../components/Input";
 export default function Basket() {
   const router = useRouter();
+  const [isCheckout, setIsCheckout] = useState(false);
 
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen relative">
+      {isCheckout && (
+        <div
+          onClick={() => setIsCheckout(false)}
+          className="fixed w-screen bg-black  opacity-40 h-screen top-0 left-0 "
+        ></div>
+      )}
+
+      <motion.div
+        initial={{ y: "100%" }}
+        animate={{ y: isCheckout ? 0 : "100%", transition: { type: "tween" } }}
+        className="fixed -bottom-5 left-0 bg-white z-40 h-[48vh] w-screen rounded-3xl flex justify-center items-center"
+      >
+        <motion.div
+          onClick={() => setIsCheckout(false)}
+          whileHover={{ scale: 0.9 }}
+          initial={{ y: "100%" }}
+          animate={{
+            y: isCheckout ? 0 : "100%",
+            transition: { type: "tween" },
+          }}
+          className="absolute -top-20 w-14 h-14 bg-white z-30 rounded-full flex justify-center items-center cursor-pointer"
+        >
+          <X size={40} />
+        </motion.div>
+        <div className="w-8/12 mx-auto flex flex-col gap-4">
+          <div>
+            <h1 className="font-bold text-md">What’s your address?</h1>
+            <Input placeholder="" className="w-full mt-2 text-gray-special" />
+          </div>
+          <div>
+            <h1 className="font-bold text-md">What’s your phone number?</h1>
+            <Input placeholder="" className="w-full mt-2 text-gray-special" />
+          </div>
+          <button className="bg-orange-main p-2 text-white rounded-md py-4 font-bold">
+            Order
+          </button>
+        </div>
+      </motion.div>
       <div className="w-full h-28 flex justify-center items-center relative ">
         <button
           onClick={() => router.push("/store")}
@@ -18,7 +60,7 @@ export default function Basket() {
         </button>
         <h1 className="text-xl text-gray-main font-bold">Basket</h1>
       </div>
-      <BasketContent />
+      <BasketContent setIsCheckout={setIsCheckout} />
     </main>
   );
 }
